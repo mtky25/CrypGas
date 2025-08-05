e1b7bdaff3b415dc806bef8afad89a2fe1115931
mtky25 <mtky25@usp.br>
2025-08-04 23:15:33 -0300

    feat: blowfish implementado

--- Crypgas/blowfish.c
+++ Crypgas/blowfish.c
@@ -1,175 +1,126 @@
-#include <stdio.h>
-#include <stdint.h>
+#include "blowfish.h"
+#include "blowfish_tables.h"
 #include <string.h>
-#include <stdlib.h>
 
-#define N 16
-
-// Tabela P e S-boxes (valores iniciais, simplificados)
-uint32_t P[18] = {
-    0x243F6A88, 0x85A308D3, 0x13198A2E, 0x03707344,
-    0xA4093822, 0x299F31D0, 0x082EFA98, 0xEC4E6C89,
-    0x452821E6, 0x38D01377, 0xBE5466CF, 0x34E90C6C,
-    0xC0AC29B7, 0xC97C50DD, 0x3F84D5B5, 0xB5470917,
-    0x9216D5D9, 0x8979FB1B
-};
-
-uint32_t S[4][256] = {
-    {0xD1310BA6, 0x98DFB5AC, 0x2FFD72DB, 0xD01ADFB7, 0xB8E1AFED, 0x6A267E96, 0xBA7C9045, 0xF12C7F99},
-    {0x4B7A70E9, 0xB5B32944, 0xDB75092E, 0xC4192623, 0xAD6EA6B0, 0x49A7DF7D, 0x9CEE60B8, 0x8FEDB266},
-    {0x78AF2FDA, 0x55605C60, 0xE65525F3, 0xAA55AB94, 0x57489862, 0x63E81440, 0x55CA396A, 0x2AAB10B6},
-    {0x6B8FE4D6, 0x99F73FD6, 0xA1D29C07, 0xEFE830F5, 0x4D2D38E6, 0xF0255DC1, 0x4CDD2086, 0x8470EB26}
-    // ... complete as S-boxes para uso real ...
-};
-
-uint32_t F(uint32_t x) {
-    // Exemplo simplificado: ideal seria usar S-boxes reais
-    return ((x >> 24) ^ (x >> 16) ^ (x >> 8) ^ x);
+/* Função F */
+static inline uint32_t F(blowfish_ctx_t *ctx, uint32_t x) {
+    uint8_t a = (x >> 24) & 0xFF;
+    uint8_t b = (x >> 16) & 0xFF;
+    uint8_t c = (x >> 8)  & 0xFF;
+    uint8_t d = x & 0xFF;
+    return ((ctx->S[0][a] + ctx->S[1][b]) ^ ctx->S[2][c]) + ctx->S[3][d];
 }
 
-void blowfish_encrypt(uint32_t *left, uint32_t *right) {
-    int i;
+void blowfish_init(blowfish_ctx_t *ctx, const uint8_t *key, uint32_t key_len) {
+    memcpy(ctx->P, P_init, sizeof(P_init));
+    memcpy(ctx->S, S_init, sizeof(S_init));
+
+    uint32_t key_index = 0;
+    for (int i = 0; i < 18; i++) {
+        uint32_t data = 0;
+        for (int k = 0; k < 4; k++) {
+            data = (data << 8) | key[key_index];
+            key_index = (key_index + 1) % key_len;
+        }
+        ctx->P[i] ^= data;
+    }
+
+    uint32_t left = 0, right = 0;
+    for (int i = 0; i < 18; i += 2) {
+        blowfish_encrypt_block(ctx, &left, &right);
+        ctx->P[i] = left;
+        ctx->P[i + 1] = right;
+    }
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 256; j += 2) {
+            blowfish_encrypt_block(ctx, &left, &right);
+            ctx->S[i][j] = left;
+            ctx->S[i][j + 1] = right;
+        }
+    }
+}
+
+void blowfish_encrypt_block(blowfish_ctx_t *ctx, uint32_t *left, uint32_t *right) {
     uint32_t L = *left;
     uint32_t R = *right;
-
-    for (i = 0; i < N; i++) {
-        L ^= P[i];
-        R ^= F(L);
-        uint32_t temp = L;
-        L = R;
-        R = temp;
+    for (int i = 0; i < 16; i++) {
+        L ^= ctx->P[i];
+        R ^= F(ctx, L);
+        uint32_t temp = L; L = R; R = temp;
     }
-    uint32_t temp = L;
-    L = R;
-    R = temp;
-
-    R ^= P[N];
-    L ^= P[N + 1];
-
+    uint32_t temp = L; L = R; R = temp;
+    R ^= ctx->P[16];
+    L ^= ctx->P[17];
     *left = L;
     *right = R;
 }
 
-void blowfish_decrypt(uint32_t *left, uint32_t *right) {
+void blowfish_decrypt_block(blowfish_ctx_t *ctx, uint32_t *left, uint32_t *right) {
     uint32_t L = *left;
     uint32_t R = *right;
-
-    for (int i = N + 1; i > 1; i--) {
-        L ^= P[i];
-        R ^= F(L);
-        uint32_t temp = L;
-        L = R;
-        R = temp;
+    for (int i = 17; i > 1; i--) {
+        L ^= ctx->P[i];
+        R ^= F(ctx, L);
+        uint32_t temp = L; L = R; R = temp;
     }
-    uint32_t temp = L;
-    L = R;
-    R = temp;
-
-    R ^= P[1];
-    L ^= P[0];
-
+    uint32_t temp = L; L = R; R = temp;
+    R ^= ctx->P[1];
+    L ^= ctx->P[0];
     *left = L;
     *right = R;
 }
 
-void print_block(uint32_t left, uint32_t right) {
-    printf("%08X %08X\n", left, right);
+/* PKCS#7 padding helper */
+static uint32_t apply_pkcs7(uint8_t *buffer, uint32_t len) {
+    uint8_t pad = BLOWFISH_BLOCK_SIZE - (len % BLOWFISH_BLOCK_SIZE);
+    if (pad == 0) pad = BLOWFISH_BLOCK_SIZE; // adiciona bloco extra
+    for (uint8_t i = 0; i < pad; i++)
+        buffer[len + i] = pad;
+    return len + pad;
 }
 
-// Função CBC para criptografar
-void blowfish_encrypt_cbc(uint8_t *data, size_t len) {
-    uint32_t iv_left = 0x12345678;
-    uint32_t iv_right = 0x9ABCDEF0;
-    for (size_t i = 0; i < len; i += 8) {
-        uint32_t left = 0, right = 0;
-        memcpy(&left, data + i, 4);
-        memcpy(&right, data + i + 4, 4);
+/* CBC com padding seguro (processa bloco a bloco) */
+void blowfish_encrypt_cbc_padded(blowfish_ctx_t *ctx,
+                                 const uint8_t *input,
+                                 uint32_t input_len,
+                                 uint8_t *output,
+                                 uint32_t *output_len,
+                                 const uint8_t iv[BLOWFISH_BLOCK_SIZE]) {
+    // calcula tamanho final com padding
+    uint8_t temp[256]; // suporta até 256 bytes (pode aumentar se precisar)
+    if (input_len > 248) input_len = 248; // garante espaço para padding
 
-        // CBC: XOR com IV ou bloco anterior
-        left ^= iv_left;
-        right ^= iv_right;
+    memcpy(temp, input, input_len);
+    uint32_t padded_len = apply_pkcs7(temp, input_len);
 
-        blowfish_encrypt(&left, &right);
+    uint8_t prev[8];
+    memcpy(prev, iv, 8);
 
-        // Salva bloco cifrado
-        memcpy(data + i, &left, 4);
-        memcpy(data + i + 4, &right, 4);
+    // processa cada bloco de 8 bytes
+    for (uint32_t i = 0; i < padded_len; i += 8) {
+        uint8_t block[8];
+        memcpy(block, temp + i, 8);
 
-        // Atualiza IV para próximo bloco
-        iv_left = left;
-        iv_right = right;
+        // XOR com bloco anterior ou IV
+        for (int j = 0; j < 8; j++)
+            block[j] ^= prev[j];
+
+        uint32_t L = (block[0]<<24)|(block[1]<<16)|(block[2]<<8)|block[3];
+        uint32_t R = (block[4]<<24)|(block[5]<<16)|(block[6]<<8)|block[7];
+
+        blowfish_encrypt_block(ctx, &L, &R);
+
+        output[i+0] = (L >> 24) & 0xFF;
+        output[i+1] = (L >> 16) & 0xFF;
+        output[i+2] = (L >> 8)  & 0xFF;
+        output[i+3] = L & 0xFF;
+        output[i+4] = (R >> 24) & 0xFF;
+        output[i+5] = (R >> 16) & 0xFF;
+        output[i+6] = (R >> 8)  & 0xFF;
+        output[i+7] = R & 0xFF;
+
+        memcpy(prev, output + i, 8);
     }
+
+    *output_len = padded_len;
 }
-
-// Função CBC para descriptografar
-void blowfish_decrypt_cbc(uint8_t *data, size_t len) {
-    uint32_t iv_left = 0x12345678;
-    uint32_t iv_right = 0x9ABCDEF0;
-    for (size_t i = 0; i < len; i += 8) {
-        uint32_t left = 0, right = 0, c_left = 0, c_right = 0;
-        memcpy(&left, data + i, 4);
-        memcpy(&right, data + i + 4, 4);
-
-        c_left = left;
-        c_right = right;
-
-        blowfish_decrypt(&left, &right);
-
-        // CBC: XOR com IV ou bloco anterior cifrado
-        left ^= iv_left;
-        right ^= iv_right;
-
-        // Salva bloco decifrado
-        memcpy(data + i, &left, 4);
-        memcpy(data + i + 4, &right, 4);
-
-        // Atualiza IV para próximo bloco
-        iv_left = c_left;
-        iv_right = c_right;
-    }
-}
-
-// int main() {
-//     char frase[256];
-//     printf("Digite uma frase (até 255 caracteres):\n");
-//     fgets(frase, sizeof(frase), stdin);
-
-//     size_t len = strlen(frase);
-//     if (frase[len - 1] == '\n') frase[len - 1] = '\0';
-//     len = strlen(frase);
-
-//     // Padding para múltiplo de 8 bytes
-//     size_t padded_len = ((len + 7) / 8) * 8;
-//     uint8_t *buffer = calloc(padded_len, 1);
-//     memcpy(buffer, frase, len);
-
-//     // Vetor de inicialização (IV) fixo para exemplo
-//     uint32_t iv_left = 0x12345678;
-//     uint32_t iv_right = 0x9ABCDEF0;
-
-//     printf("\nTexto original (hex):\n");
-//     for (size_t i = 0; i < padded_len; i += 8) {
-//         uint32_t left = 0, right = 0;
-//         memcpy(&left, buffer + i, 4);
-//         memcpy(&right, buffer + i + 4, 4);
-//         print_block(left, right);
-//     }
-
-//     blowfish_encrypt_cbc(buffer, padded_len);
-
-//     printf("\nCriptografado (hex):\n");
-//     for (size_t i = 0; i < padded_len; i += 8) {
-//         uint32_t left = 0, right = 0;
-//         memcpy(&left, buffer + i, 4);
-//         memcpy(&right, buffer + i + 4, 4);
-//         print_block(left, right);
-//     }
-
-//     blowfish_decrypt_cbc(buffer, padded_len);
-
-//     printf("\nDescriptografado:\n");
-//     printf("%s\n", buffer);
-
-//     free(buffer);
-//     return 0;
-// }
--- Crypgas/blowfish.h
+++ Crypgas/blowfish.h
@@ -1,10 +1,27 @@
 #ifndef BLOWFISH_H
 #define BLOWFISH_H
 
-void blowfish_encrypt(uint32_t *left, uint32_t *right);
-void blowfish_decrypt(uint32_t *left, uint32_t *right);
-void print_block(uint32_t left, uint32_t right);
-void blowfish_encrypt_cbc(uint8_t *data, size_t len, uint32_t iv_left, uint32_t iv_right);
-void blowfish_decrypt_cbc(uint8_t *data, size_t len, uint32_t iv_left, uint32_t iv_right);
+#include <stdint.h>
+
+#define BLOWFISH_BLOCK_SIZE 8
+
+typedef struct {
+    uint32_t P[18];
+    uint32_t S[4][256];
+} blowfish_ctx_t;
+
+void blowfish_init(blowfish_ctx_t *ctx, const uint8_t *key, uint32_t key_len);
+void blowfish_encrypt_block(blowfish_ctx_t *ctx, uint32_t *left, uint32_t *right);
+void blowfish_decrypt_block(blowfish_ctx_t *ctx, uint32_t *left, uint32_t *right);
+
+/**
+ * Encriptação CBC com padding PKCS#7
+ */
+void blowfish_encrypt_cbc_padded(blowfish_ctx_t *ctx,
+                                 const uint8_t *input,
+                                 uint32_t input_len,
+                                 uint8_t *output,
+                                 uint32_t *output_len,
+                                 const uint8_t iv[BLOWFISH_BLOCK_SIZE]);
 
 #endif
